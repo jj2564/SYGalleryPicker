e08387eb53b79758ce104f72e9e370cbeb4ace60
IrvingHuang <S409896@sinyi.com.tw>
2019-11-05 10:41:43 +0800

    Initial Commit

---
+++ SYGalleryPicker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SYGalleryPicker
+//
+//  Created by IrvingHuang on 2019/11/5.
+//  Copyright © 2019 Sinyi Realty Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
