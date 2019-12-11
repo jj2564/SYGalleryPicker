8c1cfced4ca1ec38a9d7f185b9d46f3747ba99eb
jj2564 <jamek8@gmail.com>
2019-12-11 12:02:12 +0800

    add missing files

---
+++ SYGalleryPicker/Classes/SYGalleryPicker/View/AlbumTableView.swift
@@ -0,0 +1,20 @@
+//
+//  AlbumTableView.swift
+//  Pods-SYGalleryPicker_Example
+//
+//  Created by IrvingHuang on 2019/12/11.
+//
+
+import UIKit
+
+class AlbumTableView: UITableView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
