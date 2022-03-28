485a047a13ec2a5f97e70c87431377a1a42966d5
IrvingHuang <s409896@sinyi.com.tw>
2022-03-28 15:34:26 +0800

    - 新增 支援SPM

---
+++ Sources/SYGalleryPicker/SYGalleryPicker.swift
@@ -0,0 +1,6 @@
+public struct SYGalleryPicker {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
---
+++ Tests/SYGalleryPickerTests/SYGalleryPickerTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import SYGalleryPicker
+
+final class SYGalleryPickerTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(SYGalleryPicker().text, "Hello, World!")
+    }
+}
